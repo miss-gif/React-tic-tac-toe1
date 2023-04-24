2003d7beed791b251b35fb7773628076ca799231
miss <ddc.akari@gmail.com>
2023-04-25 02:44:44 +0900

    tic-tac-toe

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Board from "./components/Board";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="game">
+      <div className="game__board">
+        <Board></Board>
+      </div>
+      <div className="game__info">
+        <div>game__info</div>
+      </div>
     </div>
   );
 }
